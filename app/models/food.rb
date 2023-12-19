1e340ce845deeb040d24bb79c1f77cb497097bde
ITurres <emanueliturres.co@gmail.com>
2023-12-19 15:43:07 -0300

    Feat: add 'food#total_price' model-method

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,8 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
+
+  def total_price
+    (price * quantity).round(2)
+  end
 end
