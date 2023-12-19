f22c1f7db36166ba8653fa2b3bfdee887cf19d3e
ITurres <emanueliturres.co@gmail.com>
2023-12-19 18:49:28 -0300

    Fix: foods_controller#index to fetch foods by user id

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,7 +4,7 @@ class FoodsController < ApplicationController
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @foods = Food.where(user_id: current_user.id)
   end
 
   # GET /foods/1 or /foods/1.json
