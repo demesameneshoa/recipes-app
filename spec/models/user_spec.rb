f5ad9198d5c46a87562d3d11e6f288cf064c7b61
demesameneshoa <demesameneshoa@gmail.com>
2023-12-21 22:42:52 +0300

    test: add unit test for the 'user' model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      user = build(:user, name: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  describe 'associations' do
+    it 'has many recipes' do
+      user = create(:user)
+      recipe1 = create(:recipe, user: user)
+      recipe2 = create(:recipe, user: user)
+
+      expect(user.recipes).to include(recipe1, recipe2)
+    end
+  end
+
+  describe 'Devise modules' do
+    it 'is database authenticatable' do
+      user = create(:user)
+      expect(user.valid_password?('123456')).to be(true)
+      expect(user.valid_password?('wrong_password')).to be(false)
+    end
+  end
 end
