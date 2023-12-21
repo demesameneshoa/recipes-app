f3581e48a4b47346f78b54a45a04b6690dc523c6
demesameneshoa <demesameneshoa@gmail.com>
2023-12-21 10:50:52 +0300

    fix: resolve rubocop offenses

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -5,5 +5,4 @@ class RecipeFood < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :recipe_id, presence: true
   validates :food_id, presence: true
-  
 end
